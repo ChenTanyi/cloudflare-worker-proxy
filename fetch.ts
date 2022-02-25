b68e3d6edfd1c46570d2d7668edd5378bfa0245d
ChenTanyi <chentanyi@gmail.com>
2022-02-25 10:39:14 +0800

    feat: return real ip if not a valid request

--- fetch.ts
+++ fetch.ts
@@ -1,8 +1,18 @@
-async function handle(request: Request): Promise<Response> {
+async function handleFetch(request: Request): Promise<Response> {
     let url = new URL(request.url);
     let requestUrl = url.pathname + url.search;
     requestUrl = requestUrl.replace(/^\/*(https?:)(\/*)/, '$1//');
     return fetch(requestUrl, request);
+}
+
+async function handle(request: Request): Promise<Response> {
+    console.log(request.url);
+    let url = new URL(request.url);
+    if (url.pathname.startsWith('/http:/') || url.pathname.startsWith('/https:/')) {
+        return handleFetch(request);
+    } else {
+        return new Response(request.headers.get('x-real-ip'));
+    }
 }
 
 export default {
