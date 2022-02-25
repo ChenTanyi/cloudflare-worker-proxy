881a4d602c22220628109d2531f95327756836e2
ChenTanyi <chentanyi@gmail.com>
2022-02-25 10:58:24 +0800

    feat: exclude cf additional headers

--- fetch.ts
+++ fetch.ts
@@ -1,7 +1,20 @@
+let excludeHeadersPrefix = ['cf-', 'x-forwarded-proto', 'x-real-ip', 'true-client-ip', 'x-forwarded-for'];
+
 async function handleFetch(request: Request): Promise<Response> {
     let url = new URL(request.url);
     let requestUrl = url.pathname + url.search;
     requestUrl = requestUrl.replace(/^\/*(https?:)(\/*)/, '$1//');
+    let newHeaders = new Headers();
+    for (let [key, value] of request.headers.entries()) {
+        let exclude = false;
+        for (let excludePrefix in excludeHeadersPrefix) {
+            if (key.toLowerCase().startsWith(excludePrefix)) {
+                exclude = true;
+                break;
+            }
+        }
+        if (!exclude) newHeaders.append(key, value);
+    }
     return fetch(requestUrl, request);
 }
 
